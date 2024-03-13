9412205f9b23c60b4bacfee7f6d565406f9a5ae6
DragonTheory <80736+dragontheory@users.noreply.github.com>
2024-03-13 16:48:50 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,3 +11,16 @@ domReady(() => {
   // Display body when DOM is loaded
   document.body.style.opacity = '1.0';
 });
+
+
+
+// Element that clears
+let clearButton = document.querySelector("main panel-list ul li");
+
+// Element that is cleared
+let clearMe = document.querySelector("aside panel-list");
+
+// Clear the content of the element when the button is clicked
+clearButton.onclick = function() {
+  clearMe.innerHTML = "";
+};
